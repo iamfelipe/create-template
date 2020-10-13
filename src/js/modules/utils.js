311882bff5741681b91b9ff7104f03fb64940f47
Felipe Castillo <fcastillo@serempre.com>
2020-10-13 18:22:16 -0500

    ScrollsTop custom function.

--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -1,1 +1,16 @@
 export const sum = (a, b) => a + b;
+
+export const scrollsTop = () => {
+  if (window) {
+    try {
+      // The new API
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    } catch (error) {
+      window.scrollTo(0, 0);
+    }
+  }
+};
